e3321baeb867572c023e5b6da02f5306d90ad7b5
Maika Kanaka <maika-kanaka@protonmail.com>
2019-12-29 20:23:02 +0700

    api login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,26 +1,59 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import $ from 'jquery';
+
+import { HomePage } from '../home/home';
+import { environment as ENV } from '../../environments/environment';
 
 @IonicPage()
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
-export class LoginPage {
+export class LoginPage
+{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) 
+  email_or_username: string;
+  password: string;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams
+  )
   {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+
+  }
+
+  doLogin()
+  {
+    var navCtrlCopy = this.navCtrl;
+
+    $.ajax({
+      method: "POST",
+      url: ENV.api_url + "/sys/user/login",
+      dataType: "json",
+      data: {
+        email_or_username: this.email_or_username,
+        password: this.password
+      },
+      success: function(res){
+        if(res.valid){
+          localStorage.setItem('is_login', 'true');
+          localStorage.setItem('jwt', res.jwt);
+
+          localStorage.setItem('user_fullname', res.user.user_fullname);
+          localStorage.setItem('user_email', res.user.user_email);
+
+          navCtrlCopy.setRoot(HomePage);
+        }else{
+          window.alert(res.message);
+        }
+      }
+    });
   }
 
 }
