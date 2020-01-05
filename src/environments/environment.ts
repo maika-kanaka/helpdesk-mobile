972c9f46d4b66da365fb56ea03afa077a58c9da0
Maika Kanaka <maika-kanaka@protonmail.com>
2020-01-05 22:34:20 +0700

    yes, i'm using hotspot from my android :D

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export var environment = {   //THIS IS FOR DEFAULT VALUES
-  web_url: "http://localhost/utavi/web/public",
-  api_url: "http://localhost/utavi/web/public/api"
+  web_url: "http://192.168.42.100/utavi/web/public",
+  api_url: "http://192.168.42.100/utavi/web/public/api"
 }
