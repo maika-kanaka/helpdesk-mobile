ccac38da3658ef6de112b6a186798aba5608ed8f
Maika Kanaka <maika-kanaka@protonmail.com>
2020-03-15 13:43:59 +0700

    fix bugs android 9 api must be https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 import { HttpHeaders } from '@angular/common/http';
 
 export var environment = {   //THIS IS FOR DEFAULT VALUES
-  web_url: "http://192.168.42.205/utavi/web/public",
-  api_url: "http://192.168.42.205/utavi/web/public/api"
+  web_url: "https://demos.migunesia.com/helpdesk-api/public",
+  api_url: "https://demos.migunesia.com/helpdesk-api/public/api"
 }
 
 export const httpOptions = {
